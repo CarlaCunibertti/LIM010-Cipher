7086af90c845d3ad399ce1d675ef0df3e0e6e653
CarlaCunibertti <ccunibertti@outlook.com>
2019-06-06 08:04:25 -0500

    Avance primera pantalla con tres intentos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-/* Acá va tu código */
+//Botones
+const btnEmpezar = document.getElementById('btnEmpezar');
+const btnCifrar = document.getElementById('btnCifrar');
+const btnDescifrar = document.getElementById('btnDescifrar');
+const btnEmpezardenuevo = document.getElementById('btnEmpezardenuevo');
+//Vistas
+const vista0 = document.getElementById('vistaEstaticaUp');
+const vista1 = document.getElementById('vistaPrimera');
+const vista2 = document.getElementById('vistaSegunda');
+const vista3 = document.getElementById('vistaEstaticaDown');
+//Contraseña
+let login = 0;
+//Funcion primer boton Empezar
+    
+btnEmpezar.addEventListener('click', () => {
+        const contraseña = document.getElementById('traerContraseña');
+        const contraseñaValue = document.getElementById('traerContraseña').value;
+        if (login <= 2) 
+        {
+                if (contraseñaValue === 'LABORATORIA') 
+                {
+                        document.getElementById('bienvenida').innerHTML = "Bienvenidos";
+                        vista1.classList.add('hide');
+                        vista2.classList.remove('hide');
+                }
+                else 
+                {
+                        document.getElementById('resultado1').innerHTML = "Contraseña errada";
+                        login++;
+                }
+        } else 
+        {
+                document.getElementById('resultado2').innerHTML = "Lo sentimos, no puedes ingresar";
+        }
+}
+
+);
+//btnCifrar.addEventListener('click', () => {
+  //      const wordWrite=document.getElementById('palabra');
+    //    const wordwriteValue=document.getElementById('palabra').value;
+//let input2 = documeNT.getElementById('palabra');
+//let input3 = documeNT.getElementById('number');
+//let input4 = documeNT.getElementById('resultado');
+
+//rescatando el valor del input text para ser cifrado
+
+//const btnCifrado= document.getElementById("btnCifrar");
+//btnCifrar.addEventListener('click', () =>{
+//        let textEncode=document.getElementById('palabra').value;
+  //      let numberEscogido= document.getElementById('number').value;
+    //    document.getElementById('msg-encode-check').innerHTML = "PRUEBA";
+//});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
